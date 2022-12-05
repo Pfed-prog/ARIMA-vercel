1e4c0ae692011886c301e15c4ec16de3981da34e
pfed-prog <fedorovn19@gmail.com>
2022-12-06 00:17:11 +0100

    should be working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ARIMA-vercel
+
+https://medium.com/analytics-vidhya/linear-regression-in-python-from-scratch-with-scipy-statsmodels-sklearn-da8e373cc89b
--- vercel-python/index.py
+++ vercel-python/index.py
@@ -1,9 +1,12 @@
 """nonempty"""
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
+
 
 FORWARD_STEPS = 10
 
