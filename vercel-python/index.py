5ef245a2eb226419fa3f8baeacb22e94f593f38d
pfed-prog <fedorovn19@gmail.com>
2022-12-05 23:34:04 +0100

    added lib

--- vercel-python/index.py
+++ vercel-python/index.py
@@ -1,14 +1,9 @@
 """nonempty"""
-from os.path import join
-import json
-from datetime import datetime
 import requests
 from flask import Flask, request, jsonify, send_file
 import pandas as pd
 
 app = Flask(__name__)
-
-URL ='https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
 
 FORWARD_STEPS = 10
 
@@ -18,34 +13,17 @@ def get_data(address):
 
     address: address of the Uniswap contract
     """
+    response = requests.get(f"https://api.covalenthq.com/v1/pricing/historical_by_addresses_v2/9001/USD/{address}/?from=2022-10-01&key=ckey_3c25e5ced5f74d099e39692d87a")
+    data = response.json()
+    date, price = [], []
 
-    query = f"""
-        {{token (id: "{address}"){{tokenDayData(first:1000, skip:2) {{ priceUSD date open close high low volume volumeUSD}} }} }}
-    """
-    response = requests.post(URL, json={'query': query}, timeout=7.5)
-    json_data = json.loads(response.text)
-    data = json_data['data']['token']['tokenDayData']
-    out_df = pd.DataFrame(data)
+    for x in range(len(data['data'][0]['prices'])):
+        price.append(data['data'][0]['prices'][x]['price'])
+        date.append(data['data'][0]['prices'][x]['date'])
 
-    return out_df
-
-
-def input_cells(latest_date, steps=FORWARD_STEPS):
-    """
-    outputs cells that we use to input predictions
-    """
-    empty_predictions = pd.DataFrame({'date':[n*86400+latest_date for n in range(1, steps+1)]})
-    return empty_predictions
-
-
-def time_features(df):
-    """generate time features"""
-    df["utc"] = df.date.apply(lambda x: datetime.utcfromtimestamp(x))
-    df['day'] = df.utc.apply(lambda time: time.day)
-    df["weekday"] = df.utc.apply(lambda time: time.dayofweek)
-    df['month'] = df.utc.apply(lambda time: time.month)
-    df['year'] = df.utc.apply(lambda time: time.year).astype("category")
-    return df
+    dataframe = pd.DataFrame({'date': date, 'price': price})
+    dataframe = dataframe.sort_values(by='date').reset_index(drop=True)
+    return dataframe
 
 @app.route("/", methods=["GET","POST"])
 def home():
@@ -53,24 +31,8 @@ def home():
     if request.args.get('a'):
         address = request.args.get('a')
     else:
-        address = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
+        address = "0x3f75ceabCDfed1aCa03257Dc6Bdc0408E2b4b026"
 
     df_data = get_data(address)
-    empty_predictions = input_cells(df_data.date.values[-1:][0])
-    df_all = pd.concat([df_data, empty_predictions]).reset_index(drop=True)
-    df_withfeatures = time_features(df_all)
 
-    columns = ['open', 'close', 'high', 'low', 'volume', 'volumeUSD']
-    for column in columns:
-        df_withfeatures[column] = df_withfeatures[column].shift(FORWARD_STEPS)
-
-    features = ['priceUSD', 'date', 'open', 'close', 'high', 'low', 'volume',
-       'volumeUSD', 'day', 'weekday', 'month', 'year']
-
-    df_withfeatures = df_withfeatures.iloc[-FORWARD_STEPS:][features]
-
-
-    #for column in ['day', 'weekday', 'month', 'year']:
-    #    df_all[column] = df_all[column].astype(int)
-
-    return df_withfeatures.to_html(header="true", table_id="table")
+    return jsonify(df_data)
