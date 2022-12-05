b5c2bd4f9406c4051afc95016f1e781e0d728229
pfed-prog <fedorovn19@gmail.com>
2022-12-05 23:58:14 +0100

    should be working

--- vercel-python/index.py
+++ vercel-python/index.py
@@ -2,11 +2,24 @@
 import requests
 from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np
 
 app = Flask(__name__)
 
 FORWARD_STEPS = 10
+
+
+def lin_regr_func(x, y):
+    """
+    linear regression
+    """
+    n = len(x)
+    # denominator
+    d = (n*(x**2).sum()-x.sum()**2)
+    # intercept
+    a = (y.sum()*(x**2).sum()-x.sum()*(x*y).sum()) / d
+    # slope
+    b = (n*(x*y).sum()-x.sum()*y.sum())/d
+    return a, b
 
 def get_data(address):
     """
@@ -18,12 +31,13 @@ def get_data(address):
     data = response.json()
     date, price = [], []
 
-    for x in range(len(data['data'][0]['prices'])):
-        price.append(data['data'][0]['prices'][x]['price'])
-        date.append(data['data'][0]['prices'][x]['date'])
+    for day in range(len(data['data'][0]['prices'])):
+        price.append(data['data'][0]['prices'][day]['price'])
+        date.append(data['data'][0]['prices'][day]['date'])
 
     dataframe = pd.DataFrame({'date': date, 'price': price})
     dataframe = dataframe.sort_values(by='date').reset_index(drop=True)
+    dataframe['price_1'] = dataframe.price.shift(1)
     return dataframe
 
 @app.route("/", methods=["GET","POST"])
@@ -36,7 +50,11 @@ def home():
 
     df_data = get_data(address)
 
-    something = list(df_data.price[-5:])
+    intercept, coef = lin_regr_func(df_data.price_1, df_data.price)
+    predictions = []
+    last_value = df_data.price.iloc[-1]
+    for _ in range(FORWARD_STEPS):
+        predictions.append(intercept + coef*last_value)
+        last_value = intercept + coef*last_value
 
-
-    return jsonify({'data': something})
+    return jsonify({'data': predictions})
